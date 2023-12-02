6916208903f6e63cf9e3abd205c9665c3d763296
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2023-12-02 15:48:24 +0100

    Add: Main/Header

---
+++ kabel/src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <img src=""/>
+    <h1>
+        Kabel nieee
+    </h1>
+</template>
+  
+  <script>
+  export default {
+    name: 'SiteHeader',
+    props: {}
+  }
+  </script>
+  
+  <style scoped lang="scss">
+  
+  </style>
---
+++ kabel/src/components/mainPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <SiteHeader/>
+    <main>
+    
+    </main>
+</template>
+  
+<script>
+    import SiteHeader from "./header.vue"
+    
+    export default {
+        name: 'MainPage',
+        props: {}, 
+        components: {
+            SiteHeader,
+        }
+    } 
+  </script>
+  
+  <style scoped lang="scss">
+ 
+  </style>
